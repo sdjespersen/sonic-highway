a65099e5b6b7f46f94f5481fbcd76fe104004405
Scott Jespersen <sjespersen@myst.ai>
2025-05-02 17:34:01 -0700

    New Canva site.
    Still some kinks to be worked out, e.g. site background.

---
+++ _assets/0d823583b9de8b91.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[53684],{
+
+/***/ 810695:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(813110);self._7ccc7343da2ae983607b3819c7bc1f93 = self._7ccc7343da2ae983607b3819c7bc1f93 || {};(function(__c) {__c.bGb=function(a){const b={...a.props};a.ua.Py(({Tk:c,XI:d})=>{c=a.IBb?{}:{We:c()};a.na.track(a.event,{...c,Gq:d(),...b})})};__c.GJ.prototype.jU=__c.fa(17,function(){});__c.SL.prototype.jU=__c.fa(16,function(){this.ended||this.rM||(this.rM=!0,setTimeout(()=>{try{this.ended=!0,__c.nYa(this)}catch(a){this.H.Nb(a,{Ve:"Error ending span in next cycle",extra:new Map(__c.JL(this))})}}))});
+}).call(self, self._7ccc7343da2ae983607b3819c7bc1f93);}
+
+}])
+//# sourceMappingURL=sourcemaps/0d823583b9de8b91.js.map
---
+++ _assets/f6f11e68e508e8c2.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[3593],{
+
+/***/ 878415:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(813110);self._7ccc7343da2ae983607b3819c7bc1f93 = self._7ccc7343da2ae983607b3819c7bc1f93 || {};(function(__c) {var jJb,kJb;jJb=function(a){return(b,c)=>({kind:2,name:a,D6a:b,E6a:c})};kJb=function(a){return b=>({kind:3,name:a,args:b})};__c.lJb=jJb(1);__c.M_=jJb(3);__c.mJb=kJb(5);__c.nJb=kJb(0);
+}).call(self, self._7ccc7343da2ae983607b3819c7bc1f93);}
+
+}])
+//# sourceMappingURL=sourcemaps/f6f11e68e508e8c2.js.map
